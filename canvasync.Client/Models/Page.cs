70434ec0f330b7c6d3515d85aabba6d6e622a287
seoshinwoo <seoshinwoo@gmail.com>
2025-08-26 08:32:16 +0900

    ing2

--- canvasync.Client/Models/Page.cs
+++ canvasync.Client/Models/Page.cs
@@ -1,3 +1,5 @@
+using SkiaSharp;
+
 namespace Models;
 
 public class Page
@@ -5,6 +7,7 @@ public class Page
     public string PDFId { get; set; } = string.Empty;
     public int PageIndex { get; set; } = 0;
     public string ImgData { get; set; } = string.Empty;
+    // public SKImage? Image { get; set; }
     public List<Factor>? Factors { get; set; }
     public int Width { get; set; } = 0;
     public int Height { get; set; } = 0;

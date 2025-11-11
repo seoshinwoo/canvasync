4ae8901025bc8e55fded04acb3efd01a722508dc
seoshinwoo <seoshinwoo@gmail.com>
2025-11-11 21:26:16 +0900

    - BaseAddress 지정 - 펜 굵기 범위 변경

--- canvasync/Program.cs
+++ canvasync/Program.cs
@@ -9,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // builder.Services.AddHttpClient();
 builder.Services.AddHttpClient("ServerAPI", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:5175"); // 서버 주소 고정
+    // client.BaseAddress = new Uri("https://localhost:5175"); // 서버 주소 고정
+    client.BaseAddress = new Uri("https://canvasync.azurewebsites.net"); // 서버 주소 고정
 });
 
 builder.Services.AddControllers();
